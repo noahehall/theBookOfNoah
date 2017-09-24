8aaed79a82c54060164704a6820e4f29e64b4b66
noah edward hall <nhall@mdsol.com>
2017-09-24 09:10:55 -0700

    update pack scripts

--- web_apps/node/src/web/app/client/components/root/App.js
+++ web_apps/node/src/web/app/client/components/root/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
     return (
       <BrowserRouter>
         <div style={styles.app}>
-werdfgs
+werafsgsfddfgs
         </div>
       </BrowserRouter>
     )
