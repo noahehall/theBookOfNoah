fc968b6b6e8f3688ebcf0bbee29dd5290e54f845
noah edward hall <noahedwardhall@gmail.com>
2019-12-12 09:35:23 -0500

    updates

--- web_technoligies/core/workers/examples/hijack-fetch-requests.js
+++ web_technoligies/core/workers/examples/hijack-fetch-requests.js
@@ -9,6 +9,25 @@ self.addEventListener('fetch', (event) => {
 // respond with whatever was returend previously, keyed by request URL 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
+    // request API
+    // event.request.url
+    // event.request.method
+    // event.request.headers
+    // event.request.body
     caches.match(event.request)
   );
+});
+
+// example sending a request when the(fuck) cache fails 
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// return an arbitrary response with headers 
+new Response('<p>Hello from your friendly neighbourhood service worker!</p>', {
+  headers: { 'Content-Type': 'text/html' }
 });
