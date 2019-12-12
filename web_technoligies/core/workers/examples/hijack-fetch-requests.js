cca6fe4138bbda74d270892a1d02a93b59f154e3
noah edward hall <noahedwardhall@gmail.com>
2019-12-12 09:36:50 -0500

    udpates

--- web_technoligies/core/workers/examples/hijack-fetch-requests.js
+++ web_technoligies/core/workers/examples/hijack-fetch-requests.js
@@ -27,6 +27,20 @@ self.addEventListener('fetch', (event) => {
   );
 });
 
+// handle failure, make new request, update cache, then respond 
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request).then((response) => {
+        return caches.open('v1').then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });  
+      });
+    })
+  );
+});
+
 // return an arbitrary response with headers 
 new Response('<p>Hello from your friendly neighbourhood service worker!</p>', {
   headers: { 'Content-Type': 'text/html' }
