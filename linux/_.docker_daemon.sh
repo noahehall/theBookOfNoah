bcac8306c279fc6401d44d0e830e73824ed99382
noah the balboa <noahedwardhall@gmail.com>
2020-12-01 13:41:06 -0500

    updates

--- linux/_.docker_daemon.sh
+++ linux/_.docker_daemon.sh
@@ -4,6 +4,20 @@
 # https://github.com/moby/moby/blob/master/contrib/init/systemd/docker.service
 # https://docs.docker.com/engine/reference/commandline/dockerd/
 
+
+dockerdup() {
+	echo 'enter your password to start dockerd'
+	# https://docs.docker.com/engine/install/linux-postinstall/
+	# docker daemon always runs as root unless in rootless mode
+	# we still run docker as non-root users tho
+	# TODO: think this needs nohup
+	# but not to bad as the script ignores ctrlc
+	# need to grab the pid of the bg process and create a func to shut|restart it
+	"$(sudo -b -E dockerd --config-file $(getpath '_.docker_default.json') ) &"
+	echo $?
+}
+export dockerdup;
+
 # https://gist.github.com/peterver/ca2d60abc015d334e1054302265b27d9
 rep="$(curl -s --unix-socket /var/run/docker.sock http://ping > /dev/null)"
 status=$?
@@ -26,10 +40,5 @@ fi
 
 # https://tldp.org/LDP/abs/html/comparison-ops.html#STRTEST
 if [[ ${dontlaunchdockerd:-} != true ]]; then
-	# https://docs.docker.com/engine/install/linux-postinstall/
-	# docker daemon always runs as root unless in rootless mode
-	# we still run docker as non-root users tho
-	echo 'enter your password to start dockerd'
-	"$(sudo -b -E dockerd --config-file $(getpath '_.docker_default.json') ) &"
-	echo $?
+	dockerdup
 fi
