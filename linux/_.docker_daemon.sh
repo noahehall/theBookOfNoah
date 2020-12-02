b77878cb08e8e414f6e771ca2b21bd10811fe7cb
noah the balboa <noahedwardhall@gmail.com>
2020-12-01 20:16:30 -0500

    updates

--- linux/_.docker_daemon.sh
+++ linux/_.docker_daemon.sh
@@ -38,8 +38,10 @@ dockerdlogstacktrace() {
 	fi
 }
 
+# get logs
 alias dockerdlog="$(echo journalctl -u docker.service)"
-
+# get netstats (use ss on ubuntu)
+alias dockerdss="(sudo ss -asmpex | grep dockerd)"
 
 # need to log result
 dockerdown() {
