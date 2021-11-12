ab9b24a5d3b25c893b37500f84956a990bd53832
noahedwardhall <noahehall@users.noreply.github.com>
2021-11-12 06:57:08 -0500

    blah broke it

--- linux/_sourceme_.sh
+++ linux/_sourceme_.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # ^ you really want that to point to /bin/dash, but whatev
 
-[ -f ./.bash_aliases.sh ] && source ./.bash_aliases.sh
-[ -f ./.bash_functions.sh ] && source ./.bash_functions.sh
-[ -f ./.bash_variables.sh ] && source ./.bash_variables.sh
+[ -f .bash_aliases.sh ] && . .bash_aliases.sh
+[ -f .bash_functions.sh ] && . .bash_functions.sh
+[ -f .bash_variables.sh ] && . .bash_variables.sh
