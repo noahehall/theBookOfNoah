e78cb43a9f870eaa731905ce777c81779196eef2
noahedwardhall <noahehall@users.noreply.github.com>
2021-11-20 10:45:10 -0500

    finalize node install script

--- linux/.install_node.sh
+++ linux/.install_node.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
-#
-# delete the following
-# ^ ~/.{npm,nvm,pnpm,pnpm-store}
-# ^ /usr/bin
+# defaults when installing via apt
 # /usr/bin/node # default binary
 # /usr/bin/nodejs # symlink > node
 
@@ -12,11 +9,15 @@ DISTRO='linux-x64'
 NODE_DIR='/opt/nodejs'
 NODE_VER='node-v17.1.0'
 
+# fkn remove everything to get a blank system
 sudo rm -rf "$NODE_DIR"
-sudo rm -rf /usr/bin/{node,nodejs,corepack,npm,npx}
-sudo rm -rf /usr/local/bin/{node,nodejs,corepack,npm,npx}
+sudo rm -rf /usr/bin/{node,nodejs,corepack,npm,npx,pnpm}
+sudo rm -rf /usr/local/bin/{node,nodejs,corepack,npm,npx,pnpm}
+sudo rm -rf ~/.node
+sudo rm -rf ~/.local/share/pnpm-global
 
 sudo mkdir -p "$NODE_DIR"
+sudo chown $(whoami):$(whoami) $NODE_DIR
 
 curl -o node.tar.xz https://nodejs.org/dist/v17.1.0/$NODE_VER-$DISTRO.tar.xz
 
@@ -33,6 +34,7 @@ echo -e "setting symlink: $NODE_BIN"
 # symlink all node bin files as bin files /usr/bin
 sudo ln -ns $NODE_BIN/* /usr/local/bin
 
-# install pnpm
+# install pnpm & yarn via corepack
 sudo corepack enable
-corepack prepare pnpm@6.22.2 --activate
+# corepack prepare pnpm@6.22.2 --activate
+# ^ doesnt activate
