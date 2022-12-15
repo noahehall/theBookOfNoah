896547afb1d9804170440d4adaa39953e18e338a
noahehall <noahehall@users.noreply.github.com>
2022-12-15 12:09:04 -0700

    convert dk aliases to fns

--- linux/bash_cli_fns/docker.sh
+++ linux/bash_cli_fns/docker.sh
@@ -10,28 +10,57 @@ export DOCKER_FORMAT="ID\t{{.ID}}\nNAME\t{{.Names}}\nIMAGE\t{{.Image}}\nPORTS\t{
 dk_start_registry() {
     docker run --rm -d -p 5001:5001 --restart=always --name registry registry:2
 }
+
 dk_start_bash() {
     docker run --rm -it ubuntu:trusty bash
     # ip addr show eth0 # get container ip
     # route # get host IP
 }
+
 dk_start_bash_host() {
     docker run --rm -it --network host ubuntu:trusty bash
 }
 
-alias dk_ls_images='docker image ls --format="table {{.Repository}}\t{{.Size}}"'
-alias dk_see_me='$(echo docker run --rm -it alpine ping -c4 $(whatsmyip))'
-alias dk_ps="docker ps --no-trunc -a --format 'table {{.Names}}\n\t{{.Image}}\n\t{{.Status}}\n\t{{.Command}}\n\n' | tac"
-alias dk_d_remote_url="sudo netstat -lntp | grep dockerd"
-alias dk_logs='$(echo journalctl -u docker.service)'
+dk_ls_images() {
+    docker image ls --forma="table {{.Repository}}\t{{.Size}}"
+
+}
+
+dk_see_me() {
+    docker run --rm -it alpine ping -c4 $(whatsmyip)
+}
+
+dk_ps() {
+    docker ps --no-trunc -a --format 'table {{.Names}}\n\t{{.Image}}\n\t{{.Status}}\n\t{{.Command}}\n\n' | tac
+}
+
+dk_d_remote_url() {
+    sudo netstat -lntp | grep dockerd
+}
+
+dk_logs() {
+    journalctl -u docker.service
+}
+
 # get netstats (use ss on ubuntu)
-alias dl_d_ss="(sudo ss -asmpex | grep dockerd)"
+dl_d_ss() {
+    sudo ss -asmpex | grep dockerd
+}
+
 #echo image1 image2 three | xargall docker pull
-alias dk_inspect="docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}'"
+dk_inspect() {
+    docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}'
+}
+
 # see volumes for a container
-alias dk_container_volumes="docker inspect -f '{{range .Mounts}}{{println .Source}}{{println .Destination}}readWrite: {{.Mode}}{{println .RW}}{{end}}'"
+dk_container_volumes() {
+    docker inspect -f '{{range .Mounts}}{{println .Source}}{{println .Destination}}readWrite: {{.Mode}}{{println .RW}}{{end}}'
+}
+
 # get get ip addr for container
-alias dk_container_network="docker inspect -f '{{range .NetworkSettings.Networks}}{{println .IPAddress}}{{end}}'"
+dk_container_network() {
+    docker inspect -f '{{range .NetworkSettings.Networks}}{{println .IPAddress}}{{end}}'
+}
 
 dk_rm_containers_sigterm() {
     docker stop $(docker ps -aq)
