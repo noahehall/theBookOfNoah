37efe956b6dbc03f8287d8b8d80297eb5bfa32c1
noah <noah.hall@precisionplanting.com>
2023-04-02 08:22:54 -0500

    basics

--- 0current/languages/javascript/examples/maps.mjs
+++ 0current/languages/javascript/examples/maps.mjs
@@ -5,8 +5,12 @@
 
 
 
+- new Map([iterable]): The Map object holds key-value pairs. Any value (both objects and primitive values) may be used as either a key or a value.
+- properties
+  - Map.prototype.size
 
 
+- new WeakMap([iterable]): The WeakMap object is a collection of key/value pairs in which the keys are weakly referenced. The keys must be objects and the values can be arbitrary values.
 
 
 
--- 0current/languages/javascript/examples/sets.mjs
+++ 0current/languages/javascript/examples/sets.mjs
@@ -1,4 +1,19 @@
 /*
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
 - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/WeakSet
+
+
+
+
+
+- new Set([iterable]): The Set object lets you store unique values of any type, whether primitive values or object references.
+
+- new WeakSet([iterable]); If an iterable object is passed, all of its elements will be added to the new WeakSet. null is treated as undefined.
+
+
+
+
+
+
+
 */
