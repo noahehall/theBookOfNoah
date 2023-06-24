91c9a8a11c7853f1d005ca12f1e60143a003edf2
poop <noahedwardhall@gmail.com>
2023-06-24 18:58:30 -0500

    fix fkn ubuntu audio

--- linux/bash_cli_fns/audio.sh
+++ linux/bash_cli_fns/audio.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# $ alsomixer
-# $ F6 > select device
-# left/right arrow keys, M to toggle mute
-audio_open_mixer() {
+audio_open_alsamixer() {
+  # $ F6 > select device
+  # left/right arrow keys, M to toggle mute
   alsamixer
+}
+audio_open_pavucontrol() {
+  # make sure pulseaudio is configured correctly
+  pavucontrol
 }
 audio_list_devices() {
   sudo lshw -C sound
