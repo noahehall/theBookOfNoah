137bcf3f1e6e8cf7af7e65f7bf692776b9bd65d8
poop <noahedwardhall@gmail.com>
2023-10-21 21:36:02 -0700

    add typescript explorer to vscode ext installs

--- vscode.install.extensions.sh
+++ vscode.install.extensions.sh
@@ -36,6 +36,7 @@ code-insiders --force --install-extension ms-toolsai.vscode-jupyter-cell-tags
 code-insiders --force --install-extension ms-toolsai.vscode-jupyter-slideshow
 code-insiders --force --install-extension ms-vscode.cmake-tools
 code-insiders --force --install-extension ms-vscode.cpptools
+code-insiders --force --install-extension mxsdev.typescript-explorer
 code-insiders --force --install-extension naumovs.color-highlight
 code-insiders --force --install-extension nimsaem.nimvscode
 code-insiders --force --install-extension oderwat.indent-rainbow
